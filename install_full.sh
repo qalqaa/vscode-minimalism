f4c5fc12824180f0d51bb8bfa19e7411541485a6
qalqa <andreybas04@gmail.com>
2025-02-25 06:20:07 +0300

    feat: added full script + disable post install scripts

--- install_full.sh
+++ install_full.sh
@@ -17,99 +17,144 @@ echo "║                WELCOME TO QALQA'S VSCODE SETUP SCRIPT                 
 echo "╚═══════════════════════════════════════════════════════════════════════╝"
 echo ""
 
+# Список обязательных расширений
 default_extensions=(
-    "be5invis.vscode-custom-css"
-    "catppuccin.catppuccin-vsc"
-    "catppuccin.catppuccin-vsc-icons"
+  "be5invis.vscode-custom-css"
+  "catppuccin.catppuccin-vsc"
+  "catppuccin.catppuccin-vsc-icons"
+  "aaron-bond.better-comments"
+  "angular.ng-template"
+  "antfu.unocss"
+  "astro-build.astro-vscode"
+  "batisteo.vscode-django"
+  "bierner.github-markdown-preview"
+  "bierner.markdown-checkbox"
+  "bierner.markdown-emoji"
+  "bierner.markdown-footnotes"
+  "bierner.markdown-mermaid"
+  "bierner.markdown-preview-github-styles"
+  "bierner.markdown-yaml-preamble"
+  "bmewburn.vscode-intelephense-client"
+  "bradlc.vscode-tailwindcss"
+  "brandonkirbyson.vscode-animations"
+  "cardinal90.multi-cursor-case-preserve"
+  "catppuccin.catppuccin-vsc"
+  "catppuccin.catppuccin-vsc-icons"
+  "codeium.codeium"
+  "cyrilletuzi.angular-schematics"
+  "darkriszty.markdown-table-prettify"
+  "dbaeumer.vscode-eslint"
+  "donjayamanne.githistory"
+  "dsznajder.es7-react-js-snippets"
+  "ecmel.vscode-html-css"
+  "editorconfig.editorconfig"
+  "esbenp.prettier-vscode"
+  "firsttris.vscode-jest-runner"
+  "formulahendry.code-runner"
+  "foxundermoon.shell-format"
+  "fractalbrew.backticks"
+  "franklinteixeira205.primeflex"
+  "genuitecllc.codetogether"
+  "github.vscode-pull-request-github"
+  "glenn2223.live-sass"
+  "htmlhint.vscode-htmlhint"
+  "iceworks-team.iceworks-style-helper"
+  "illixion.vscode-vibrancy-continued"
+  "irongeek.vscode-env"
+  "jebbs.plantuml"
+  "jeff-hykin.better-dockerfile-syntax"
+  "jeff-hykin.better-shellscript-syntax"
+  "jeff-hykin.polacode-2019"
+  "jock.svg"
+  "kamikillerto.vscode-colorize"
+  "kevinrose.vsc-python-indent"
+  "lior-chamla.google-fonts"
+  "mgmcdermott.vscode-language-babel"
+  "mrmlnc.vscode-scss"
+  "ms-azuretools.vscode-docker"
+  "ms-python.black-formatter"
+  "ms-python.debugpy"
+  "ms-python.python"
+  "ms-python.vscode-pylance"
+  "ms-vscode-remote.remote-containers"
+  "ms-vscode-remote.remote-wsl"
+  "mskelton.npm-outdated"
+  "mylesmurphy.prettify-ts"
+  "oderwat.indent-rainbow"
+  "orta.vscode-jest"
+  "prisma.prisma"
+  "pucelle.vscode-css-navigation"
+  "pulkitgangwar.nextjs-snippets"
+  "quicktype.quicktype"
+  "redhat.vscode-xml"
+  "ritwickdey.liveserver"
+  "rvest.vs-code-prettier-eslint"
+  "sdras.vue-vscode-snippets"
+  "sibiraj-s.vscode-scss-formatter"
+  "streetsidesoftware.code-spell-checker"
+  "streetsidesoftware.code-spell-checker-russian"
+  "tonybaloney.vscode-pets"
+  "vitest.explorer"
+  "vue.volar"
+  "wix.vscode-import-cost"
+  "xabikos.javascriptsnippets"
+  "yoavbls.pretty-ts-errors"
 )
-
-declare -a optional_extensions
-
-ask_to_install() {
-    local extension_name="$1"
-    local extension_id="$2"
-
-    echo -n "Install $extension_name? (Y/N): "
-    while true; do
-        read -n1 -s key
-        case "$key" in
-        [Yy])
-            printf "\e[32m✓\e[0m\n"
-            optional_extensions+=("$extension_id")
-            break
-            ;;
-        [Nn])
-            printf "\e[31mx\e[0m\n"
-            break
-            ;;
-        esac
-    done
-}
-
-ask_to_install "Animations (VSCode Animations)" "brandonkirbyson.vscode-animations"
-ask_to_install "Transparency (Vibrancy Continued)" "illixion.vscode-vibrancy-continued"
-
-if [ ${#optional_extensions[@]} -gt 0 ]; then
-    for ext in "${optional_extensions[@]}"; do
-        code --install-extension "$ext"
-    done
-fi
 
 echo "Installing default extensions..."
 for ext in "${default_extensions[@]}"; do
-    code --install-extension "$ext"
+  code --install-extension "$ext"
 done
 
 echo ""
 printf "\e[32m══════════════════════════════════════════════\e[0m\n"
 printf "\e[32m      ALL EXTENSIONS HAVE BEEN INSTALLED      \e[0m\n"
 printf "\e[32m══════════════════════════════════════════════\e[0m\n"
-echo ""
 
 echo "Are you using the standard VSCode settings layout? (Y/N): "
 read -n1 -s std_answer
 echo
 if [[ $std_answer =~ [Yy] ]]; then
-    OS_TYPE=$(uname)
+  OS_TYPE=$(uname)
 
-    case "$OS_TYPE" in
-    "Linux")
-        USER_SETTINGS_DIR="$HOME/.config/Code/User"
-        ;;
-    "Darwin")
-        USER_SETTINGS_DIR="$HOME/Library/Application Support/Code/User"
-        ;;
-    "MINGW32_NT"* | "MINGW64_NT"* | "CYGWIN"*)
-        USER_SETTINGS_DIR="${APPDATA:-$HOME/AppData/Roaming}/Code/User"
-        ;;
-    *)
-        echo "Unknown operating system, please input the full path to the settings directory manually!"
-        read -p "Input the full path to the VSCode settings directory: " USER_SETTINGS_DIR
-        ;;
-    esac
+  case "$OS_TYPE" in
+  "Linux")
+    USER_SETTINGS_DIR="$HOME/.config/Code/User"
+    ;;
+  "Darwin")
+    USER_SETTINGS_DIR="$HOME/Library/Application Support/Code/User"
+    ;;
+  "MINGW32_NT"* | "MINGW64_NT"* | "CYGWIN"*)
+    USER_SETTINGS_DIR="${APPDATA:-$HOME/AppData/Roaming}/Code/User"
+    ;;
+  *)
+    echo "Unknown operating system, please input the full path to the settings directory manually!"
+    read -p "Input the full path to the VSCode settings directory: " USER_SETTINGS_DIR
+    ;;
+  esac
 else
-    read -p "Input the full path to the settings directory: " USER_SETTINGS_DIR
+  read -p "Input the full path to the settings directory: " USER_SETTINGS_DIR
 fi
 
 echo "User settings directory: $USER_SETTINGS_DIR"
 
-SETTINGS_FILE="./settings-ot.json"
+SETTINGS_FILE="./settings.json"
 TARGET_SETTINGS="$USER_SETTINGS_DIR/settings.json"
 
 if [ -f "$SETTINGS_FILE" ]; then
-    echo "Updating settings.json..."
-    if [ -f "$TARGET_SETTINGS" ]; then
-        tmp_file=$(mktemp)
-        jq -s '.[1] as $user | 
+  echo "Updating settings.json..."
+  if [ -f "$TARGET_SETTINGS" ]; then
+    tmp_file=$(mktemp)
+    jq -s '.[1] as $user | 
                (.[0] | to_entries | map(select(.key as $k | ($user | has($k) | not))) | from_entries) as $diff |
                $user + $diff' "$SETTINGS_FILE" "$TARGET_SETTINGS" >"$tmp_file" && mv "$tmp_file" "$TARGET_SETTINGS"
-        echo "Added settings to your settings.json!"
-    else
-        cp "$SETTINGS_FILE" "$TARGET_SETTINGS"
-        echo "settings.json not found, created new with new settings"
-    fi
+    echo "Added settings to your settings.json!"
+  else
+    cp "$SETTINGS_FILE" "$TARGET_SETTINGS"
+    echo "settings.json not found, created new with new settings"
+  fi
 else
-    echo "settings.json not found"
+  echo "settings.json not found"
 fi
 
 echo ""
@@ -120,31 +165,11 @@ echo ""
 
 CUSTOM_CSS_FILE="./qalqa-customcss.css"
 if [ -f "$CUSTOM_CSS_FILE" ]; then
-    cp "$CUSTOM_CSS_FILE" "$USER_SETTINGS_DIR/qalqa-customcss.css"
-    echo "qalqa-customcss.css copied into $USER_SETTINGS_DIR!"
+  cp "$CUSTOM_CSS_FILE" "$USER_SETTINGS_DIR/qalqa-customcss.css"
+  echo "qalqa-customcss.css copied into $USER_SETTINGS_DIR!"
 else
-    echo "qalqa-customcss.css not found"
+  echo "qalqa-customcss.css not found"
 fi
-
-# echo "Running post-installation commands..."
-
-# if [[ "$OS_TYPE" == "Linux" ]]; then
-#     xdg-open "vscode://command/extension.installCustomCSS"
-#     xdg-open "vscode://command/VSCode-Animations.enableAnimations"
-#     xdg-open "vscode://command/extension.installVibrancy"
-# elif [[ "$OS_TYPE" == "Darwin" ]]; then
-#     open "vscode://command/extension.installCustomCSS"
-#     open "vscode://command/VSCode-Animations.enableAnimations"
-#     open "vscode://command/extension.installVibrancy"
-# elif [[ "$OS_TYPE" == "MINGW32_NT"* ]] || [[ "$OS_TYPE" == "MINGW64_NT"* ]] || [[ "$OS_TYPE" == "CYGWIN"* ]]; then
-#     start "" "vscode://command/extension.installCustomCSS"
-#     start "" "vscode://command/VSCode-Animations.enableAnimations"
-#     start "" "vscode://command/extension.installVibrancy"
-# else
-#     echo "Unknown operating system, skipping post-installation commands. Please run them manually. Check the README for more info"
-# fi
-
-# echo "Post-installation commands executed successfully!"
 
 echo ""
 printf "\e[32m══════════════════════════════════════════════\e[0m\n"
